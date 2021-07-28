564bee85e4631ce01bcb42ba57fe966beee93435
Maddi <0337189@schoolsnet.act.edu.au>
2021-07-28 13:57:45 +1000

    initial creation of index.php

--- config.php
+++ config.php
@@ -3,4 +3,6 @@ session_start();
 
 $conn = new SQLite3("Maddi's Project 1") or die ("unable to open database");
 $query = file_get_contents("sql/create-user.sql");
+$stmt = $conn->prepare($query);
+
 ?>

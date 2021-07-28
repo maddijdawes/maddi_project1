44aaed63b9411934bf7027351e6e890c11a7c313
Maddi <0337189@schoolsnet.act.edu.au>
2021-07-28 15:21:20 +1000

    initial creation of index.php

--- config.php
+++ config.php
@@ -2,7 +2,20 @@
 session_start();
 
 $conn = new SQLite3("Maddi's Project 1") or die ("unable to open database");
-$query = file_get_contents("sql/create-user.sql");
-$stmt = $conn->prepare($query);
+function createTable($sqlStmt, $tableName)
+{
+    global $conn;
+    $stmt = $conn->prepare($sqlStmt);
+    if ($stmt->execute()) {
+        echo "<p style='color: greenyellow'>".$tableName.": Table Created Successfully</p>";
 
+    } else {
+        echo "<p style='color: red'>".$tableName.": Table Created Successfully<?p>";
+    }
+}
+
+$query = file_get_contents("sql/create-user.sql");
+createTable($query, "User");
+$query_01 = file_get_contents("sql/create-user-Copy.sql");
+createTable($query_01, "Messaging");
 ?>

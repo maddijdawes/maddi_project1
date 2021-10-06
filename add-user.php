8349ee1899978b7c40605314ad9a246611206bf5
maddijdawes <0337189@schoolsnet.act.edu.au>
2021-10-06 21:24:04 +1100

    created add-user.php and product-add.php

---
+++ add-user.php
@@ -0,0 +1,135 @@
+
+<?php include "template.php"; ?>
+<title>Create New User</title>
+<h1 class='text-primary'>Create New User</h1>
+
+<?php
+$query = $conn->query("SELECT DISTINCT accessLevel FROM user");
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+    <div class="container-fluid">
+        <div class="row">
+            <!--Customer Details-->
+
+            <div class="col-md-6">
+                <h2>Account Details</h2>
+                <p>Please enter wanted username and password:</p>
+                <p>User Name<input type="text" name="username" class="form-control" required="required"></p>
+                <p>Password<input type="password" name="password" class="form-control" required="required"></p>
+               <!-- <p>Access Level<input type="text" name="accessLevel" class="form-control" required="required"></p>--!>
+<p>Access Level
+    <select name="accessLevel">
+        <?php
+        while ($row = $query->fetchArray()) {
+            echo '<option>'.$row[0].'</option>';
+        }
+        ?>
+    </select>
+</p>
+            </div>
+            <div class="col-md-6">
+                <h2>More Details</h2>
+                <!--More user details-->
+                <p>Please enter More Personal Details:</p>
+                <p>Name<input type="text" name="name" class="form-control" required="required"></p>
+                <p>Email<input type="text" name="email" class="form-control" required="required"></p>
+                <p>Address<input type="text" name="address" class="form-control" required="required"></p>
+                <p>Phone<input type="text" name="phone" class="form-control" required="required"></p>
+                <p>Profile Picture <input type="file" name="file" class="form-control" required="required"></p>
+            </div>
+        </div>
+    </div>
+    <input type="submit" name="formSubmit" value="Submit">
+</form>
+
+<?php
+// If a form is submitted, the data will be sanitised
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//    Customer Details
+    $username = sanitise_data($_POST['username']);
+    $password = sanitise_data($_POST['password']);
+    $name = sanitise_data($_POST['name']);
+    $email = sanitise_data($_POST['email']);
+    $address = sanitise_data($_POST['address']);
+    $phone = sanitise_data($_POST['phone']);
+    $accessLevel = sanitise_data($_POST['accessLevel']);
+
+//check if user exists.
+    $query = $conn->query("SELECT COUNT(*) FROM user WHERE username='$username'");
+    $data = $query->fetchArray();
+    $numberOfUsers = (int)$data[0];
+
+    if ($numberOfUsers > 0) {
+        echo "Sorry, username already taken";
+    } else {
+// User Registration commences
+
+//for the user-hashed table.
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+//for the image table.
+        $file = $_FILES['file'];
+
+//Variable Names
+        $fileName = $_FILES['file']['name'];
+        $fileTmpName = $_FILES['file']['tmp_name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileError = $_FILES['file']['error'];
+        $fileType = $_FILES['file']['type'];
+
+//defining what type of file is allowed
+// We seperate the file, and obtain the end.
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+//We ensure the end is allowable in our thing.
+        $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+
+        if (in_array($fileActualExt, $allowed)) {
+            if ($fileError === 0) {
+                //File is smaller than yadda.
+                if ($fileSize < 10000000000) {
+                    //file name is now a unique ID based on time with IMG- precedding it, followed by the file type.
+                    $fileNameNew = uniqid('IMG-', True) . "." . $fileActualExt;
+                    //upload location
+                    $fileDestination = 'uploads/' . $fileNameNew;
+                    //command to upload.
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    $sql = "INSERT INTO user (username, password, name, profilePic, accessLevel) VALUES (:newUsername, :newPassword, :newName, :newImage, :newAccessLevel)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bindValue(':newUsername', $username);
+                    $stmt->bindValue(':newPassword', $hashed_password);
+                    $stmt->bindValue(':newName', $name);
+                    $stmt->bindValue(':newImage', $fileNameNew);
+                    $stmt->bindValue(':newAccessLevel', $accessLevel);
+                    $stmt->execute();
+                    header("location:index.php");
+                } else {
+                    echo "Your image is too big!";
+                }
+            } else {
+                echo "there was an error uploading your image!";
+            }
+        } else {
+            echo "You cannot upload files of this type!";
+        }
+    }
+}
+
+?>
+
+</body>
+</html>
+
+
+<?php
+//The code below sanitises code data to prevent XSS attacks
+function sanitise_data($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
---
+++ product-add.php
@@ -0,0 +1,126 @@
+
+<?php include "template.php"; ?>
+<title>Create New Product</title>
+<h1 class='text-primary'>Create New Product</h1>
+
+<?php
+$query = $conn->query("SELECT DISTINCT category FROM products");
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+    <div class="container-fluid">
+        <div class="row">
+            <!--Products Details-->
+
+            <div class="col-md-6">
+                <h2>Products Details</h2>
+                <p>Product Name<input type="text" name="prodName" class="form-control" required="required"></p>
+                <p>Category
+                    <select name="prodCategory">
+                        <?php
+                        while ($row = $query->fetchArray()) {
+                            echo '<option>'.$row[0].'</option>';
+                        }
+                        ?>
+                    </select>
+                </p>
+                <p>Quantity<input type="number" name="prodQuantity" class="form-control" required="required"></p>
+            </div>
+            <div class="col-md-6">
+                <h2>Product List</h2>
+                <!--Product List-->
+                <p>Price<input type="number" step="0.01" name="prodPrice" class="form-control" required="required"></p>
+                <p>Product Code<input type="text" name="prodCode" class="form-control" required="required"></p>
+                <p>Product Picture <input type="file" name="prodImage" class="form-control" required="required"></p>
+            </div>
+        </div>
+    </div>
+    <input type="submit" name="formSubmit" value="Submit">
+</form>
+
+<?php
+// If a form is submitted, the data will be sanitised
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//    Customer Details
+    $prodName = sanitise_data($_POST['prodName']);
+    $prodCategory = sanitise_data($_POST['prodCategory']);
+    $prodQuantity = sanitise_data($_POST['prodQuantity']);
+    $prodPrice = sanitise_data($_POST['prodPrice']);
+    $prodCode = sanitise_data($_POST['prodCode']);
+
+//check if product exists.
+    $query = $conn->query("SELECT COUNT(*) FROM products WHERE code='$prodCode'");
+    $data = $query->fetchArray();
+    $numberOfProducts = (int)$data[0];
+
+    if ($numberOfProducts > 0) {
+        echo "Sorry, username already taken";
+    } else {
+// Product Registration commences
+
+//for the image table.
+        $file = $_FILES['prodImage'];
+
+//Variable Names
+        $fileName = $_FILES['prodImage']['name'];
+        $fileTmpName = $_FILES['prodImage']['tmp_name'];
+        $fileSize = $_FILES['prodImage']['size'];
+        $fileError = $_FILES['prodImage']['error'];
+        $fileType = $_FILES['prodImage']['type'];
+
+//defining what type of file is allowed
+// We seperate the file, and obtain the end.
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+//We ensure the end is allowable in our thing.
+        $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+
+        if (in_array($fileActualExt, $allowed)) {
+            if ($fileError === 0) {
+                //File is smaller than yadda.
+                if ($fileSize < 10000000000) {
+                    //file name is now a unique ID based on time with IMG- precedding it, followed by the file type.
+                    $fileNameNew = uniqid('IMG-', True) . "." . $fileActualExt;
+                    //upload location
+                    $fileDestination = 'image/' . $fileNameNew;
+                    //command to upload.
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    $sql = "INSERT INTO products (productName, category, quantity, price, image, code) VALUES (:newProdName, :newProdCategory, :newProdQuantity, :newProdPrice, :newProdImage, :newProdCode)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bindValue(':newProdName', $prodName);
+                    $stmt->bindValue(':newProdCategory', $prodCategory);
+                    $stmt->bindValue(':newProdQuantity', $prodQuantity);
+                    $stmt->bindValue(':newProdPrice', $prodPrice);
+                    $stmt->bindValue(':newProdImage', $fileNameNew);
+                    $stmt->bindValue(':newProdCode', $prodCode);
+                    $stmt->execute();
+                    header("location:index.php");
+                } else {
+                    echo "Your image is too big!";
+                }
+            } else {
+                echo "there was an error uploading your image!";
+            }
+        } else {
+            echo "You cannot upload files of this type!";
+        }
+    }
+}
+
+?>
+
+</body>
+</html>
+
+
+<?php
+//The code below sanitises code data to prevent XSS attacks
+function sanitise_data($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
